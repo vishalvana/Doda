83079032d14a73fd206738789caa45fac81c4285
vishal vana <vishalvana592003@gmail.com>
2026-06-30 02:09:52 +0530

    chore: initialize project structure

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Doda Backend Running 🚀"
+  });
+});
+
+export default app;
